e214e36269ce1d689e9c6316506bc157cbd8af3f
roertbb <robert.banaszak1997@gmail.com>
2018-01-05 22:51:55 +0100

    server rewritten

--- inf132189_s.c
+++ inf132189_s.c
@@ -1,20 +1,6 @@
 #include "inf132189_h.h"
 
-user users[20];
-group groups[10];
-
-int num_of_users = 0,
-    num_of_groups = 0;
-
-int msgid_server, //server 1234
-    msgid_client, //client 1235
-    msgid_report; //report 1236
-
-client_msg cm;
-server_msg sm;
-report_msg rm;
-
-void load_config() {
+void load_config(user * users, group * groups, int * num_of_users, int * num_of_groups) {
     int i;
     char buf[8];
     FILE *f;
@@ -23,17 +9,17 @@ void load_config() {
     if (strcmp(buf,"USERS\n")) {
         fscanf(f, "%s", buf);
         while(strcmp(buf,"GROUPS")) {
-            users[num_of_users].pids[0] = 0;
-            users[num_of_users].pids[1] = 0;
-            strcpy(users[num_of_users++].username,buf);
+            users[*num_of_users].pids[0] = 0;
+            users[*num_of_users].pids[1] = 0;
+            strcpy(users[(*num_of_users)++].username,buf);
             fscanf(f, "%s", buf);
         }
         fscanf(f, "%s", buf);
         while(strcmp(buf,"END")) {
             for (i=0; i<20; i++) {
-                groups[num_of_groups].users[i]=0;
+                groups[*num_of_groups].users[i]=0;
             }
-            strcpy(groups[num_of_groups++].groupname, buf);
+            strcpy(groups[(*num_of_groups)++].groupname, buf);
             fscanf(f, "%s", buf);
         }
     }
@@ -53,60 +39,60 @@ void load_config() {
     // }
 }
 
-void handle_login() {
+void handle_login(int msgid_client, int msgid_report, client_msg * cm, report_msg * rm, user * users, int num_of_users) {
     int i;
     for (i=0; i<num_of_users; i++) {
-        if (strcmp(cm.receiver,users[i].username) == 0) {
+        if (strcmp(cm->receiver,users[i].username) == 0) {
             if (users[i].pids[0] > 0) {
                 //already logged in
-                rm.type = cm.pids[0];
-                rm.feedback = 2;
+                rm->type = cm->pids[0];
+                rm->feedback = 2;
                 printf("attempt to login as %s, but already logged in\n", users[i].username);
                 //[TODO]check how many times pid tried to login
-                msgsnd(msgid_report, &rm, sizeof(rm)-sizeof(long), 0);
+                msgsnd(msgid_report, rm, sizeof(report_msg)-sizeof(long), 0);
                 return;
             }
             else {
                 //login
-                rm.type = cm.pids[0];
-                rm.feedback = 1;
+                rm->type = cm->pids[0];
+                rm->feedback = 1;
                 printf("user %s logged in\n", users[i].username);
-                msgsnd(msgid_report, &rm, sizeof(rm)-sizeof(long), 0);
-                users[i].pids[0] = cm.pids[0];
-                users[i].pids[1] = cm.pids[1];
+                msgsnd(msgid_report, rm, sizeof(report_msg)-sizeof(long), 0);
+                users[i].pids[0] = cm->pids[0];
+                users[i].pids[1] = cm->pids[1];
                 return;
             }
         } 
     }
     //no user with such username
-    rm.type = cm.pids[0];
-    rm.feedback = 0;
-    printf("unsuccessful login by %d\n", cm.pids[0]);
+    rm->type = cm->pids[0];
+    rm->feedback = 0;
+    printf("unsuccessful login by %d\n", cm->pids[0]);
     //[TODO]check how many times pid tried to login
-    msgsnd(msgid_report, &rm, sizeof(rm)-sizeof(long), 0);    
+    msgsnd(msgid_report, rm, sizeof(report_msg)-sizeof(long), 0);    
 }
 
-void handle_logout() {
+void handle_logout(int msgid_client, int msgid_report, client_msg * cm, report_msg * rm, user * users, int num_of_users) {
     int i;
     for (i=0; i<num_of_users; i++) {
-        if (cm.pids[0] == users[i].pids[0]) {
-            rm.type = cm.pids[0];
-            rm.feedback = 1;
+        if (cm->pids[0] == users[i].pids[0]) {
+            rm->type = cm->pids[0];
+            rm->feedback = 1;
             users[i].pids[0] = 0;
             users[i].pids[0] = 0;
             printf("user %s logged out\n", users[i].username);
-            msgsnd(msgid_report, &rm, sizeof(rm)-sizeof(long), 0);
+            msgsnd(msgid_report, rm, sizeof(report_msg)-sizeof(long), 0);
             return;
         }
     }
     //something went wrong
-    rm.type = cm.pids[0];
-    rm.feedback = 0;
+    rm->type = cm->pids[0];
+    rm->feedback = 0;
     printf("user %s couldn't log out\n", users[i].username);
-    msgsnd(msgid_report, &rm, sizeof(rm)-sizeof(long), 0);
+    msgsnd(msgid_report, rm, sizeof(report_msg)-sizeof(long), 0);
 }
 
-void handle_request_user_list() {
+void handle_request_user_list(int msgid_server, int msgid_report, client_msg * cm, report_msg * rm, server_msg * sm, user * users, int num_of_users) {
     char username[8];
     int i;
 
@@ -123,56 +109,72 @@ void handle_request_user_list() {
         }
     }
 
-    sm.type = cm.pids[1];
-    sm.msg_type = 3;
-    strcpy(sm.text, ul);
-    msgsnd(msgid_server, &sm, sizeof(sm)-sizeof(long), 0);
+    sm->type = cm->pids[1];
+    sm->msg_type = 3;
+    strcpy(sm->text, ul);
+    msgsnd(msgid_server, sm, sizeof(server_msg)-sizeof(long), 0);
 
-    rm.type = cm.pids[0];
-    rm.feedback = 1;
-    for (i=0; i<20; i++) {
-        if (cm.pids[0] == users[i].pids[0]) {
+    rm->type = cm->pids[0];
+    rm->feedback = 1;
+    for (i=0; i<num_of_users; i++) {
+        if (cm->pids[0] == users[i].pids[0]) {
             strcpy(username, users[i].username);
         }
     }
     printf("user %s requested user list\n", username);
-    msgsnd(msgid_report, &rm, sizeof(rm)-sizeof(long), 0);
+    msgsnd(msgid_report, rm, sizeof(report_msg)-sizeof(long), 0);
 }
 
-void handle_user_message() {
+void handle_user_message(int msgid_server, int msgid_report, client_msg * cm, report_msg * rm, server_msg * sm, user * users, int num_of_users) {
     int i, j;
     char username[8];
     for (int i=0; i<num_of_users; i++) {
-        if (strcmp(cm.receiver,users[i].username) == 0) {
+        if (strcmp(cm->receiver,users[i].username) == 0) {
             //send message to user
-            sm.type = users[i].pids[1];
+            sm->type = users[i].pids[1];
 
             for (j=0; j<num_of_users; j++) {
-                if (users[j].pids[0] == cm.pids[0])
+                if (users[j].pids[0] == cm->pids[0])
                     strcpy(username, users[j].username);
             }
 
-            sm.msg_type = 9;
-            strcpy(sm.sender,username);
-            strcpy(sm.text, cm.text);
-            msgsnd(msgid_server, &sm, sizeof(sm)-sizeof(long), 0);
+            sm->msg_type = 9;
+            strcpy(sm->sender, username);
+            strcpy(sm->text, cm->text);
+            msgsnd(msgid_server, sm, sizeof(server_msg)-sizeof(long), 0);
             //confirm to client
-            rm.type = cm.pids[0];
-            rm.feedback = 1;
-            printf("user %s send message to %s\n", username, cm.receiver);
-            msgsnd(msgid_report, &rm, sizeof(rm)-sizeof(long), 0);
+            rm->type = cm->pids[0];
+            rm->feedback = 1;
+            printf("user %s send message to %s\n", username, cm->receiver);
+            msgsnd(msgid_report, rm, sizeof(report_msg)-sizeof(long), 0);
             return;
         }
     }
     //wrong username
-    rm.type = cm.pids[0];
-    rm.feedback = 0;
+    rm->type = cm->pids[0];
+    rm->feedback = 0;
     printf("user %s send message, but with wrong username\n", username);
-    msgsnd(msgid_report, &rm, sizeof(rm)-sizeof(long), 0);
+    msgsnd(msgid_report, rm, sizeof(report_msg)-sizeof(long), 0);
 }
 
 int main() {
-    load_config();
+    user * users = (user*) malloc(20*sizeof(user));
+    group * groups = (group*) malloc(10*sizeof(group));
+    // user users[20];
+    // group groups[10];
+
+    int num_of_users = 0,
+        num_of_groups = 0;
+
+    int msgid_server, //server 1234
+        msgid_client, //client 1235
+        msgid_report; //report 1236
+
+    client_msg cm;
+    server_msg sm;
+    report_msg rm;
+
+    load_config(users, groups, &num_of_users, &num_of_groups);
 
     //deleting message queues
     msgid_server = msgget(SERVER, IPC_CREAT|0644); 
@@ -193,21 +195,20 @@ int main() {
         msgrcv(msgid_client, &cm, sizeof(cm)-sizeof(long), 0, 0);
         switch(cm.type) {
             case 1:
-                handle_login();
+                handle_login(msgid_client, msgid_report, &cm, &rm, users, num_of_users);
                 break;
             case 2:
-                handle_logout();
+                handle_logout(msgid_client, msgid_report, &cm, &rm, users, num_of_users);
                 break;
             case 3:
-                handle_request_user_list();
+                handle_request_user_list(msgid_server, msgid_report, &cm, &rm, &sm, users, num_of_users);
                 break;
             case 9:
-                handle_user_message();
+                handle_user_message(msgid_server, msgid_report, &cm, &rm, &sm, users, num_of_users);
                 break;
         }
     }
-    msgrcv(msgid_server, &cm, sizeof(cm)-sizeof(long), 1, 0);
-    printf("%s\n",cm.text);
-
-    while(1);
+    
+    free(users);
+    free(groups);
 }
